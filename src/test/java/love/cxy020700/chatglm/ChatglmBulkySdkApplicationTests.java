826336a8e09a2d8235adcabaf9abf0caba40c66a
pengyanyu <1484351074@qq.com>
2023-12-09 15:07:16 +0800

    工程初始化

---
+++ src/test/java/love/cxy020700/chatglm/ChatglmBulkySdkApplicationTests.java
@@ -0,0 +1,13 @@
+package love.cxy020700.chatglm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatglmBulkySdkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
